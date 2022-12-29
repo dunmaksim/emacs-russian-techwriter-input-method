1d86134d04ecf2305969c7546ead7ad425cd7243
Максим Дунаевский <mdunaevskii@astralinux.ru>
2022-12-29 11:22:40 +0300

    Updated summary, added license info and creation date

--- russian-techwriter.el
+++ russian-techwriter.el
@@ -1,28 +1,44 @@
-;;; russian-techwriter.el --- Input method for russians technical writers
+;;; russian-techwriter.el --- Input methods for Russian technical writers
 
 ;; This input method based on GNU EMACS russian-computer input method
 
 ;; Author: Maxim Dunaevskii <dunmaksim@yandex.ru>
-;; URL: https://github.com/dunmaksim/emacs-russian-techwriter-input-method
 ;; Maintainer: Maxim Dunaevskii
+;; Created: Dec 15, 2022
 ;; Keywords: multilingual, input method, Cyrillic, i18n
 ;; Version: 0.0.2
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; URL: https://github.com/dunmaksim/emacs-russian-techwriter-input-method
+
+;; This file is not a part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; This file is not a part of GNU Emacs.
-;;
 ;; This input method optimized for fast text input with inserting some
 ;; characters without using (insert-char) function.
+;; See the README.md for details.
 
 ;;; Code:
+
+(require 'quail)
 
 (defconst russian-techwriter-version "0.0.2"
   "Input method for russian technical writers.
 
 Based on GNU EMACS russian-computer input method")
-
-(require 'quail)
 
 ;; Метод ввода для технических писателей
 (quail-define-package
